564fb3f679de3b5cfc0c8b0264bafded5cb6d2d4
Chase McElroy <ichasem@gmail.com>
2017-05-23 14:03:47 -0700

    setup firebase and geofire

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Chase McElroy on 5/23/17.
+//  Copyright © 2017 Chase McElroy. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
